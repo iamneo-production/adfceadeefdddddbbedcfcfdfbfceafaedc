8c1d0887abb9ec9a07ddf6979b30088cab659a90
Iamneo <techadmin@iamneo.ai>
2023-08-13 12:59:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,13 @@
-package com.examly.springapp; 
+package com.examly.springapp;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-public class SpringappApplication { 
+public class SpringappApplication {
 
-    Run | Debug 
-    public static void main(String[] args){
-        SpringappApplication.run(SpringappApplication)
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappApplication.class, args);
+	}
 
-    
 }
